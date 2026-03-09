ebb402b3eafed8f927d31b063bc88021c2438cb7
Nikita Pakhomov <punkhomov@gmail.com>
2026-03-09 22:01:34 +0400

    style: format code

--- examples/single/src/index.ts
+++ examples/single/src/index.ts
@@ -3,13 +3,11 @@ import { WebsoftAuth } from "@boristype/http-handler/build/auth";
 
 let counter = 0;
 
-
 const app = createApp("/api/test");
 app.use(WebsoftAuth());
 
-
 app.get("/hello-world", (req, res) => {
-  const greeting = `Hello, ${req.auth.elem?.firstname.Value ?? "Anonymous"}!`
+  const greeting = `Hello, ${req.auth.elem?.firstname.Value ?? "Anonymous"}!`;
 
   res.json({ message: greeting }, 200);
 });
@@ -24,14 +22,15 @@ app.get("/groups", (req, res) => {
 
   const conditionStr = conditions.length > 0 ? `where ${conditions.join(" and ")}` : "";
 
-  const result = ArraySelectAll(tools.xquery<GroupCatalogDocumentTopElem>(`for $elem in groups ${conditionStr} return $elem`)).map((group) => ({
+  const result = ArraySelectAll(
+    tools.xquery<GroupCatalogDocumentTopElem>(`for $elem in groups ${conditionStr} return $elem`),
+  ).map((group) => ({
     id: group.id.Value,
     name: group.name.Value,
-    counter: (counter += 1)
+    counter: (counter += 1),
   }));
 
   res.json(result, 200);
 });
-
 
 app.register();
